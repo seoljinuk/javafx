a5e4bdb0b413054f262245015f44d083730e90a8
oraman <oraman@naver.com>
2024-08-05 11:03:33 +0900

    3번째 작업

--- src/main/java/com/itgroup/application/Layout02.java
+++ src/main/java/com/itgroup/application/Layout02.java
@@ -20,6 +20,8 @@ public class Layout02 extends Application {
     }
 
     public static void main(String[] args) {
+        // 여기는 메인 메소드입니다.
+        System.out.println("aaa");
         launch(args);
     }
 }
